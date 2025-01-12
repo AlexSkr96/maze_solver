2a4588d5a25bb12593c64aaf3bef52e033adff01
Alex <github.tacky872@passmail.net>
2025-01-12 17:00:52 +0300

    Removed unused variable

--- src/TestMaze.py
+++ src/TestMaze.py
@@ -7,7 +7,7 @@ class TestMaze(unittest.TestCase):
     def test_maze_create_cells(self):
         num_rows = 12
         num_cols = 10
-        window = win = Window(800, 600)
+        window = Window(800, 600)
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, window)
         self.assertEqual(len(m1._cells), num_rows)
         self.assertEqual(len(m1._cells[0]), num_cols)
@@ -16,7 +16,7 @@ class TestMaze(unittest.TestCase):
     def test_reset_cells_visited(self):
         num_rows = 12
         num_cols = 10
-        window = win = Window(800, 600)
+        window = Window(800, 600)
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, window)
         for row in m1._cells:
             for cell in row:
